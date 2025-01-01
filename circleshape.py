a448819e388ef92641cf6c2d79a95d357e68f12b
A R C <wolverinexmen59@gmail.com>
2025-01-01 19:57:24 +0530

    B: add player.py

---
+++ circleshape.py
@@ -0,0 +1,21 @@
+import pygame
+
+# Base class for game objects
+# CircleShape extends the Sprite class to also store a position, velocity, and radius.
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius):
+
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)
+        else:
+            super().__init__()
+
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
+
+    def draw(self, screen):
+        pass
+
+    def update(self, dt):
+        pass
--- main.py
+++ main.py
@@ -1,16 +1,34 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     color = (0, 0, 0)
+
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     while (1):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, color)
+        # player.draw(screen)
+        for item in drawable:
+            item.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        # player.update(dt)
+        for item in updatable:
+            item.update(dt)
 
     print('Starting asteroids!')
     print(f'Screen width: {SCREEN_WIDTH}')
